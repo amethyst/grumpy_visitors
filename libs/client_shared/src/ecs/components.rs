dcedb2c269ea9f3d878430de911e90e6607f71b3
Hanna Rosul <hanna.rosul@gmail.com>
2019-10-20 14:47:29 +0300

    Refactor specs Component using specs-derive

--- libs/client_shared/src/ecs/components.rs
+++ libs/client_shared/src/ecs/components.rs
@@ -2,12 +2,9 @@ use amethyst::ecs::{Component, DenseVecStorage};
 
 use gv_core::math::Vector2;
 
+#[derive(Component)]
 pub struct HealthUiGraphics {
     pub screen_position: Vector2,
     pub scale_ratio: f32,
     pub health: f32,
-}
-
-impl Component for HealthUiGraphics {
-    type Storage = DenseVecStorage<Self>;
 }
