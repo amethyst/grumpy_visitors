be96a84463df9cfcc51fb70231c9859ca1e6fd1d
Hanna Rosul <hanna.rosul@gmail.com>
2019-10-20 17:10:40 +0300

    Use Logger::from_config_formatter for server

--- bins/server/src/main.rs
+++ bins/server/src/main.rs
@@ -6,12 +6,11 @@ use amethyst::{
     core::{frame_limiter::FrameRateLimitStrategy, transform::TransformBundle},
     network::{NetworkBundle, ServerConfig},
     prelude::{Application, GameDataBuilder},
-    LogLevelFilter, LoggerConfig, StdoutLog,
+    LogLevelFilter, Logger,
 };
 use laminar::Config as LaminarConfig;
-use log::LevelFilter;
 
-use std::{env, io, path::PathBuf, str::FromStr, time::Duration};
+use std::time::Duration;
 
 use gv_core::{
     ecs::resources::world::{
@@ -45,16 +44,23 @@ fn main() -> amethyst::Result<()> {
         .value_of("addr")
         .expect("Expected a default value");
 
-    Logger::from_config(Default::default())
-        .level_for("gfx_backend_vulkan", LogLevelFilter::Warn)
-        .level_for("gv_game::ecs::systems", LogLevelFilter::Debug)
-        .level_for(
-            "gv_game::ecs::systems::net_connection_manager",
-            LogLevelFilter::Info,
-        )
-        .level_for("gv_game::utils::net", LogLevelFilter::Info)
-        .level_for("gv_server", LogLevelFilter::Debug)
-        .start();
+    Logger::from_config_formatter(Default::default(), |out, message, record| {
+        out.finish(format_args!(
+            "[{level}][SERVER][{target}] {message}",
+            level = record.level(),
+            target = record.target(),
+            message = message,
+        ))
+    })
+    .level_for("gfx_backend_vulkan", LogLevelFilter::Warn)
+    .level_for("gv_game::ecs::systems", LogLevelFilter::Debug)
+    .level_for(
+        "gv_game::ecs::systems::net_connection_manager",
+        LogLevelFilter::Info,
+    )
+    .level_for("gv_game::utils::net", LogLevelFilter::Info)
+    .level_for("gv_server", LogLevelFilter::Debug)
+    .start();
 
     let mut builder = Application::build("./", LoadingState::default())?;
     builder
@@ -103,109 +109,4 @@ fn main() -> amethyst::Result<()> {
     game.run();
 
     Ok(())
-}
-
-// TODO: watch https://github.com/amethyst/amethyst/issues/1889 to get rid of this copy-pasta.
-pub struct Logger {
-    dispatch: fern::Dispatch,
-}
-
-impl Logger {
-    fn new() -> Self {
-        let dispatch = fern::Dispatch::new().format(|out, message, record| {
-            out.finish(format_args!(
-                "[{level}][SERVER][{target}] {message}",
-                level = record.level(),
-                target = record.target(),
-                message = message,
-            ))
-        });
-        Self { dispatch }
-    }
-
-    /// Create a new Logger from [`LoggerConfig`]
-    pub fn from_config(mut config: LoggerConfig) -> Self {
-        if config.allow_env_override {
-            env_var_override(&mut config);
-        }
-
-        let mut logger = Self::new();
-        logger.dispatch = logger.dispatch.level(config.level_filter);
-
-        match config.stdout {
-            StdoutLog::Plain => logger.dispatch = logger.dispatch.chain(io::stdout()),
-            StdoutLog::Colored => {
-                logger.dispatch = logger
-                    .dispatch
-                    .chain(colored_stdout(fern::colors::ColoredLevelConfig::new()))
-            }
-            StdoutLog::Off => {}
-        }
-
-        if let Some(log_gfx_device_level) = config.log_gfx_device_level {
-            logger.dispatch = logger
-                .dispatch
-                .level_for("gfx_device_gl", log_gfx_device_level);
-        }
-
-        if let Some(path) = config.log_file {
-            if let Ok(log_file) = fern::log_file(path) {
-                logger.dispatch = logger.dispatch.chain(log_file)
-            } else {
-                eprintln!("Unable to access the log file, as such it will not be used")
-            }
-        }
-
-        logger
-    }
-
-    /// Set individual log levels for modules.
-    pub fn level_for<T: Into<std::borrow::Cow<'static, str>>>(
-        mut self,
-        module: T,
-        level: LevelFilter,
-    ) -> Self {
-        self.dispatch = self.dispatch.level_for(module, level);
-        self
-    }
-
-    /// Starts [`Logger`] by consuming it.
-    pub fn start(self) {
-        self.dispatch.apply().unwrap_or_else(|_| {
-            log::debug!("Global logger already set, default Amethyst logger will not be used")
-        });
-    }
-}
-
-fn env_var_override(config: &mut LoggerConfig) {
-    if let Ok(var) = env::var("AMETHYST_LOG_STDOUT") {
-        match var.to_lowercase().as_ref() {
-            "off" | "no" | "0" => config.stdout = StdoutLog::Off,
-            "plain" | "yes" | "1" => config.stdout = StdoutLog::Plain,
-            "colored" | "2" => config.stdout = StdoutLog::Colored,
-            _ => {}
-        }
-    }
-    if let Ok(var) = env::var("AMETHYST_LOG_LEVEL_FILTER") {
-        if let Ok(lf) = LevelFilter::from_str(&var) {
-            config.level_filter = lf;
-        }
-    }
-    if let Ok(path) = env::var("AMETHYST_LOG_FILE_PATH") {
-        config.log_file = Some(PathBuf::from(path));
-    }
-}
-
-fn colored_stdout(color_config: fern::colors::ColoredLevelConfig) -> fern::Dispatch {
-    fern::Dispatch::new()
-        .chain(io::stdout())
-        .format(move |out, message, record| {
-            let color = color_config.get_color(&record.level());
-            out.finish(format_args!(
-                "{color}{message}{color_reset}",
-                color = format!("\x1B[{}m", color.to_fg_str()),
-                message = message,
-                color_reset = "\x1B[0m",
-            ))
-        })
 }
